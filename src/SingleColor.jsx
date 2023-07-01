a788014e6e24c764437f54c551ff4dd9c5bf5faf
Rupam Sutar <rupamsutar281998@gmail.com>
2023-07-01 18:45:41 +0530

    Completed with toasts

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
+import { toast } from "react-toastify";
 
-const SingleColor = ({color, index}) => {
-    const {hex, weight} = color;
+const SingleColor = ({ color, index }) => {
+  const { hex, weight } = color;
+
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color Copied to clipboard !");
+      } catch (error) {
+        toast.error("color copying failed");
+      }
+    } else {
+      toast.error("Clipboard access denied !");
+    }
+  };
+
   return (
-    <article className={index > 10 ? 'color color-light' : 'color'} style={{backgroundColor:`#${hex}`}} >
-        <p className="percent-value">{weight}</p>
-        <p className="color-value">#{hex}</p>
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ backgroundColor: `#${hex}` }}
+      onClick={saveToClipboard}
+    >
+      <p className="percent-value">{weight}</p>
+      <p className="color-value">#{hex}</p>
     </article>
-  )
-} 
+  );
+};
 
-export default SingleColor
+export default SingleColor;
