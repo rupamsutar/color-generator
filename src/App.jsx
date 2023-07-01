81cf080dd06a18718d7de0a8e1960c1d44116b51
Rupam Sutar <rupamsutar281998@gmail.com>
2023-07-01 15:45:32 +0530

    Toast package added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js";
+import {ToastContainer, toast} from 'react-toastify';
 
 const App = () => {
   const [colors, setColors] = useState(new Values('#f15025').all(10));
 
+  toast.error('error');
+
   return <main>
     <Form />
     <ColorList colors={colors}/>
+    <ToastContainer position='top-center' />
   </main>;
 };
 export default App;
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const SingleColor = ({color, index}) => {
     const {hex, weight} = color;
-    console.log(hex)
   return (
-    <article className="color" style={{backgroundColor:`#${color}`}} ></article>
+    <article className={index > 10 ? 'color color-light' : 'color'} style={{backgroundColor:`#${hex}`}} >
+        <p className="percent-value">{weight}</p>
+        <p className="color-value">#{hex}</p>
+    </article>
   )
-}
+} 
 
 export default SingleColor
